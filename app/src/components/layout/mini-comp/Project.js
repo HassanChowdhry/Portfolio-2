28664377bd2e66f997a2f3a5fcc6badc0b5fc6ff
Hassan Chowdhry <94680098+HassanChowdhry@users.noreply.github.com>
2023-07-09 10:42:25 +0500

    Info (#2)
    * all info in json file and working buttons
    
    * eslint
    
    * ...

--- app/src/components/layout/mini-comp/Project.js
+++ app/src/components/layout/mini-comp/Project.js
@@ -1,18 +1,20 @@
-function Project() {
+function Project({ title, description, src, stack }) {
   return (
-    <div className="flex flex-col justify-center w-2/3 m-auto bg-gray">
-      <section className="p-14">
-        <h2 className="mb-10 text-2xl font-bold">Google Links</h2>
-        <p className="overflow-y-clip">
-          Programmed an open-source web application that improved productivity for researchers.
-          The application scrapes google search data and parse it into an excel sheet according
-          to the research requirement.
+    <div className="flex w-2/3 m-auto bg-gray mb-16">
+      <section className="w-3/4 text-left p-14">
+        <h2 className="mb-10 text-2xl font-bold">{title}</h2>
+        <p className="mb-5 overflow-y-clip">
+          &bull; {description}
+        </p>
+        <p>
+          &bull; <b>Tech Stack: </b> {stack}
         </p>
       </section>
 
-      <section className="">
-        <iframe className="w-full" style={{ aspectRatio: '15/5' }} src="https://d17o0fjvg61p4q.cloudfront.net/gallery" title="tweet-gen" />
+      <section className="w-2/4">
+        <img className="h-full" src={src} alt={title} />
       </section>
+
     </div>
   );
 }
