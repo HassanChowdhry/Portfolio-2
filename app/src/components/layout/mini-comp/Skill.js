d67c29adcc052192f7fa39148d25a27e84b5ef86
Hassan Chowdhry <94680098+HassanChowdhry@users.noreply.github.com>
2023-07-10 06:44:16 +0500

    ...

--- app/src/components/layout/mini-comp/Skill.js
+++ app/src/components/layout/mini-comp/Skill.js
@@ -5,7 +5,7 @@ function Skill({ title, skills }) {
         <h2 className="section-title">{title}</h2>
         <div className="grid grid-cols-5">
           {skills.map(({ skill, image }) => (
-            <article className="p-8 hover:opacity-50">
+            <article className="p-8 hover:opacity-50 hover:cursor-pointer">
               <h5>{skill}</h5>
               <img src={image} alt="" />
             </article>
